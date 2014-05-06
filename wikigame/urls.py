e0c8e26f3491e0986f2b7b3a5720d4296c3d4ffa
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-05-06 20:19:28 +0200

    Implemented first draft of article crawling.

--- wikigame/urls.py
+++ wikigame/urls.py
@@ -3,5 +3,6 @@ from django.utils.translation import ugettext as _
 
 urlpatterns = patterns('',
     url(r'^$', 'wikigame.views.home', name='home'),
-    url(r'^%s$' % _('about') , 'wikigame.views.about', name='about'),
+    url(r'^%s$' % _('about'), 'wikigame.views.about', name='about'),
+    url(r'^%s/(.*)$' % _('article'), 'wikigame.views.article', name='article'),
 )
--- wikigame/views.py
+++ wikigame/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+from wikigame.link_extraction import get_links
+
 
 def home(request):
     return render(request, 'home.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
+
+def article(request, article):
+    context = {'article': article, 'links': get_links(article, 'en')}
+
+    return render(request, 'article.html', context)
