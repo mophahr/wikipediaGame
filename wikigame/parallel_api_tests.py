ddb4ab1d470c833e36f905f9aef644d0233a17e2
moritz <msch@pks.mpg.de>
2014-06-26 17:20:32 +0200

    parallel version of api test added

--- wikigame/parallel_api_tests.py
+++ wikigame/parallel_api_tests.py
@@ -3,11 +3,13 @@ import urllib.request
 import urllib.parse
 import json
 import time
+import queue as qu
+import threading as th
 
 
 start = time.time()
 
-def link_request(more_parameters={"continue": ""}):
+def link_request(queue, priority, more_parameters={"continue": ""}):
    parameters = {"format": "json",
                  "action": "query",
                  "generator": "links",
@@ -37,15 +39,28 @@ def link_request(more_parameters={"continue": ""}):
       print(json.dumps(data['warnings'], indent=4))
       exit()
 
-   return data
+   queue.put((priority,data))
+
+def update_links(queue, priority,data,query_result):
+   known_pages=set(query_result.keys())
+   recieved_pages=set(data['query']['pages'].keys())
+
+   for pageId in known_pages&recieved_pages:
+      query_result[pageId].update(data['query']['pages'][pageId])
+
+   for pageId in recieved_pages-known_pages:
+      query_result.update({pageId: data['query']['pages'][pageId]})
+
+   queue.put((priority,query_result))
+
 
 def get_link_data():
-   data=link_request()
+   link_request(queue,0)
+   data=queue.get()[1]
    
    query_result=data['query']['pages']
 
    while 'continue' in data.keys():
-      #print(json.dumps(data['continue'], indent=4))
       continue_dict=dict()
       for key in list(data['continue'].keys()):
          if key == 'continue':
@@ -53,18 +68,18 @@ def get_link_data():
          else:
             val= "|".join([urllib.parse.quote(e) for e in data['continue'][key].split('|')])
             continue_dict.update({key: val})
-      data=link_request(continue_dict)
-      
-      known_pages=set(query_result.keys())
-      recieved_pages=set(data['query']['pages'].keys())
-
-      for pageId in known_pages&recieved_pages:
-         query_result[pageId].update(data['query']['pages'][pageId])
-
-      for pageId in recieved_pages-known_pages:
-         query_result.update({pageId: data['query']['pages'][pageId]})
+      thread1=th.Thread(target=update_links, args=[queue,0,data,query_result])
+      thread1.start()
+      thread2=th.Thread(target=link_request, args=[queue,1,continue_dict])
+      thread2.start()
+      thread1.join()
+      thread2.join()
+      query_result=queue.get()[1]
+      data=queue.get()[1]
 
    #print(json.dumps(query_result, indent=4))
+
+queue=qu.PriorityQueue()
 
 start_page="Albert Einstein"
 wikipedia_language="de"
